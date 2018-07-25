b3d9616916aa9db071c81bf89c08eb2eb488192e
Arun Prasaath <39402@Hexaware.com>
2018-07-25 11:03:16 +0530

    AP : LE integration - check with html param

--- js/src/app.js
+++ js/src/app.js
@@ -644,7 +644,7 @@ define(['jquery', 'settings', 'apiService', 'utils'], function ($, config, apiSe
                     // });
                     console.log("executed");
                     console.log($("a.popover-html1").val());
-                    $("a.popover-html1").unbind().click();
+                    $("a.popover-html1").unbind().click(function(){alert("clicked")});
                     
                 },2000);
             }
