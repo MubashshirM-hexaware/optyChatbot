537f9eed246f2989e43e9333adaa49d63adedf39
Arun Prasaath <39402@Hexaware.com>
2018-07-25 10:54:02 +0530

    AP : LE integration - check with html param

--- js/src/app.js
+++ js/src/app.js
@@ -637,15 +637,16 @@ define(['jquery', 'settings', 'apiService', 'utils'], function ($, config, apiSe
             if (data.state === 'ended' && chatState !== 'ended') {
                 globalLpChat = false;
                 chat.disposeVisitor();
-                //setTimeout(function(){
+                setTimeout(function(){
                     // $("a.popover-html1").unbind().click(function (event) {
                     //     event.preventDefault();
                     //     event.stopPropagation();
                     // });
                     console.log("executed");
+                    console.log($("a.popover-html1"));
                     $("a.popover-html1").unbind().click();
                     
-                //},2000);
+                },2000);
             }
             chatState = data.state;
         }
