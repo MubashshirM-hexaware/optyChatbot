b06dbfda65e0f67fb2bff790ca3ef5cb2d4886dd
MubashshirM-hexaware <MubashshirM@hexaware.com>
2018-07-27 10:27:55 +0530

    MM : chatwidget config changes

--- webhook.js
+++ webhook.js
@@ -20,7 +20,7 @@ app.post('/callPhone', function (req, res) {
   })
 })
 app.post('/updateSessionState', function (req, res) {
-  callServiceNowApi("https://p3ep1jeoz4.execute-api.us-east-1.amazonaws.com/Dev/updatesession", { type: req.body.type }, "POST", function (err, data) {
+  callServiceNowApi("https://p3ep1jeoz4.execute-api.us-east-1.amazonaws.com/Dev/updatesession-dev", { type: req.body.type }, "POST", function (err, data) {
 
     res.send(data);
   })
