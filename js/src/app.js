b7eec84707cda2bb1cf7c74d95e6e2adaa73c620
Ashwin Ganesh <41741@hexaware.com>
2019-01-29 11:40:34 +0530

    AG: Ethisalat

--- js/src/app.js
+++ js/src/app.js
@@ -657,7 +657,7 @@
                          var textF = line.text;
                          var textI = textF.replace('<div dir="ltr" style="direction: ltr; text-align: left;">', '');
                          textF = textI.replace('</div>', '');
-                         if (!textF.includes('Ethibot says :')) {
+                         if (!textF.includes('Etibot says :')) {
                              var html_div = '<li class="animated fadeInLeft list-group-item background-color-custom"><table border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;"><img width="35" height="35" src="avatar/logo-large.png"/></td><td><div class="media-body bot-txt-space"><p class="list-group-item-text-bot">' + textF + '</p><p class="bot-res-timestamp"><small> <img style="border-radius:50%;border:2px solid white;" width="20" height="20" src="./avatar/bot-logo-image.png"/>' + utils.currentTime() + '</small></p></div></td></tr></table></li>';
                              if (msg_container.hasClass('hidden')) { // can be optimimzed and removed from here
                                  msg_container.siblings("h1").addClass('hidden');
@@ -877,7 +877,7 @@
          }
 
          function offline() {
-             var html_div = '<li class="animated fadeInLeft list-group-item background-color-custom"><table border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;"><img width="35" height="35" src="avatar/logo-large.png"/></td><td><div class="media-body bot-txt-space"><p class="list-group-item-text-bot">Sorry our Ethibot agent service is offline. Please email us your queries queries@ethisala.ae </p><p class="bot-res-timestamp"><small> <img style="border-radius:50%;border:2px solid white;" width="20" height="20" src="./avatar/bot-logo-image.png"/>' + utils.currentTime() + '</small></p></div></td></tr></table></li>';
+             var html_div = '<li class="animated fadeInLeft list-group-item background-color-custom"><table border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;"><img width="35" height="35" src="avatar/logo-large.png"/></td><td><div class="media-body bot-txt-space"><p class="list-group-item-text-bot">Sorry our Etibot agent service is offline. Please email us your queries queries@ethisala.ae </p><p class="bot-res-timestamp"><small> <img style="border-radius:50%;border:2px solid white;" width="20" height="20" src="./avatar/bot-logo-image.png"/>' + utils.currentTime() + '</small></p></div></td></tr></table></li>';
              if (msg_container.hasClass('hidden')) { // can be optimimzed and removed from here
                  msg_container.siblings("h1").addClass('hidden');
                  msg_container.siblings("div.chat-text-para").addClass('hidden');
